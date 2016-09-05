d37cc99b7dc54deb64da13716eca3c626b528a60
ted <postmaster@oneteque.com>
2016-09-06 04:35:22 +0800

    initial commit

--- CircularGlowExample/ViewController.swift
+++ CircularGlowExample/ViewController.swift
@@ -1,25 +1,40 @@
-//
-//  ViewController.swift
-//  CircularGlowExample
-//
-//  Created by ted gao on 6/9/2016.
-//  Copyright © 2016 ted gao. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var targetView: UIView!
+    
+    @IBAction func addGlow(sender: UIButton)
+    {
+        let insetDistance = targetView.bounds.size.width / 4.0
+        let insetRect = CGRectInset(targetView.bounds, insetDistance, insetDistance / 3.0)
+        let maskPath = UIBezierPath(ovalInRect: insetRect)
+        
+        let layerPath = UIBezierPath(ovalInRect: targetView.bounds)
+        layerPath.appendPath(maskPath)
+        layerPath.usesEvenOddFillRule = true
+        
+        let shadowLayer = CAShapeLayer()
+        
+        shadowLayer.fillColor = UIColor.purpleColor().CGColor
+        shadowLayer.path = layerPath.CGPath
+        shadowLayer.shadowColor = UIColor.purpleColor().CGColor
+        shadowLayer.shadowRadius = 15.0
+        shadowLayer.shadowOpacity = 0.8
+        shadowLayer.shadowOffset = CGSizeMake(0.0, 0.0)
+        shadowLayer.fillRule = kCAFillRuleEvenOdd
+        
+        targetView.layer.addSublayer(shadowLayer)
+    }
+    
+    @IBAction func clearGlow(sender: UIButton)
+    {
+        targetView.layer.sublayers?.forEach{ $0.removeFromSuperlayer() }
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
